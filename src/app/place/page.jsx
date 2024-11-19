0a6cd40efd480efb8daf2c4e87ff9f30c35a27e5
parkyejin <uiop5809@naver.com>
2024-11-19 14:03:53 +0900

    feat(#5): 페이지 route 설정

---
+++ src/app/place/page.jsx
@@ -0,0 +1,5 @@
+const PlacePage = () => {
+  return <div>댕플페이지</div>;
+};
+
+export default PlacePage;
---
+++ src/app/user/page.jsx
@@ -0,0 +1,5 @@
+const UserPage = () => {
+  return <div>댕플페이지</div>;
+};
+
+export default UserPage;

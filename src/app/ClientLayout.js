24be58010021ba7245f829fa106235826633bb41
parkyejin <uiop5809@naver.com>
2024-11-21 10:33:22 +0900

    design(#13): 서비스 접속 화면 UI

--- src/app/ClientLayout.js
+++ src/app/ClientLayout.js
@@ -2,18 +2,24 @@
 
 import { usePathname } from "next/navigation";
 import { ThemeProvider } from "styled-components";
+import { useEffect, useState } from "react";
 import BottomNav from "@/components/common/NavBottom/NavBottom";
 import theme from "@/styles/theme";
 import "./globals.css";
 
 export default function ClientLayout({ children }) {
+  const [isClient, setIsClient] = useState(false);
   const pathname = usePathname();
-  const noBottomNavPaths = ["/login"];
+  const noBottomNavPaths = ["/", "/login"];
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
       {children}
-      {!noBottomNavPaths.includes(pathname) && <BottomNav />}
+      {isClient && !noBottomNavPaths.includes(pathname) && <BottomNav />}
     </ThemeProvider>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,73 @@
+"use client";
+
+import styled from "styled-components";
+import Image from "next/image";
+
 export default function Home() {
-  return <div>메인페이지</div>;
+  return (
+    <Container>
+      <TextBox>
+        <SubTitle>
+          <p>반려견과 함께 떠나는</p>
+          <p>특별한 공간</p>
+        </SubTitle>
+        <Title>댕댕플레이스</Title>
+      </TextBox>
+
+      <ButtonBox>
+        <Image
+          src="/assets/common/kakao_login_btn.svg"
+          width={300}
+          height={50}
+          alt="kakao_login_btn"
+        />
+        <Image
+          src="/assets/common/google_login_btn.svg"
+          width={300}
+          height={50}
+          alt="google_login_btn"
+        />
+      </ButtonBox>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 60px;
+
+  background-image: url("/assets/common/start.svg");
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+  color: ${({ theme }) => theme.colors.white};
+`;
+
+const TextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  margin-top: 10rem;
+`;
+
+const ButtonBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  img {
+    cursor: pointer;
+  }
+`;
+
+const SubTitle = styled.h2`
+  text-align: center;
+  font-size: 1.4rem;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  font-size: 3.3rem;
+`;

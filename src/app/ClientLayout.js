e487b4892ecc58084ccfb8b7aeb12bebdcef0026
parkyejin <uiop5809@naver.com>
2024-11-25 22:49:51 +0900

    feat(#13): SelectBox 공통 컴포넌트

--- src/app/ClientLayout.js
+++ src/app/ClientLayout.js
@@ -10,7 +10,7 @@ import "./globals.css";
 export default function ClientLayout({ children }) {
   const [isClient, setIsClient] = useState(false);
   const pathname = usePathname();
-  const noBottomNavPaths = ["/", "/signin"];
+  const noBottomNavPaths = ["/", "/signin", "/signin/info"];
 
   useEffect(() => {
     setIsClient(true);

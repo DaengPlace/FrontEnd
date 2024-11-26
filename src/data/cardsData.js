64d3a3280563da91b3db59888bccd416adcc0e80
KangBoSeok <kbs9901@gmail.com>
2024-11-26 16:46:20 +0900

    Style(#19) : 데이터 추가

---
+++ src/data/cardsData.js
@@ -0,0 +1,25 @@
+export const cards = [
+    {
+      id: 1,
+      title: "92버터샵",
+      category: "카페",
+      rating: "5.0",
+      address: "경상북도 구미시 산호대로33길 18",
+      hours: "운영시간 11:00 ~ 20:00 | 월요일, 일요일 휴무",
+      features: "주차 가능 | 실내공간 | 무게 제한 없음 | 애견 동반 요금 없음",
+      image: "/assets/image 19.png",
+      isLiked: false,
+    },
+    {
+        id: 2,
+        title: "92버터샵",
+        category: "카페",
+        rating: "5.0",
+        address: "경상북도 구미시 산호대로33길 18",
+        hours: "운영시간 11:00 ~ 20:00 | 월요일, 일요일 휴무",
+        features: "주차 가능 | 실내공간 | 무게 제한 없음 | 애견 동반 요금 없음",
+        image: "/assets/image 19.png",
+        isLiked: false,
+    },
+  ];
+

c9e82ad7caf5c3b2c9680a05ed6049834e60744d
김예원 <yaewon4643@naver.com>
2024-11-27 02:14:40 +0900

    Design(#26): 4개 헤더 생성 완료

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,38 +1,51 @@
 import React from 'react';
 import styled from "styled-components";
-import {ChevronLeft} from "@styled-icons/boxicons-regular/ChevronLeft";
+import {ChevronLeft} from "@styled-icons/bootstrap/ChevronLeft";
 import {Place} from "@styled-icons/material-rounded/Place";
-import {Heart} from "@styled-icons/evaicons-solid/Heart";
+import {HeartOutlined} from "@styled-icons/entypo/HeartOutlined";
 import {Home} from "@styled-icons/feather/Home";
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import theme from "@/styles/theme.js";
+import Image from 'next/image';
+import Logo from "@/../../public/assets/mainpage/logoPlace.png";
 
-const Header = ({title, showFavoriteIcon, showMapIcon}) => {
+const Header = ({title, showLogo, showFavoriteIcon, showMapIcon}) => {
 
   const router = useRouter();
 
   return (
     <Container>
-      
-      <IconWrapper onClick={() => router.back()}>
-        <ChevronLeft size="24" />
-      </IconWrapper>
+      <SmallContainer>
 
-      <Title>{title}</Title>
+        {showLogo ? (
+          <LogoWrapper onClick={() => router.push("/main")}>
+            <LogoImage src={Logo} width={36} height={36} />
+            <LogoTitle>댕댕플레이스</LogoTitle>
+          </LogoWrapper>
+        ) : (
+          <>
+            <IconWrapper onClick={() => router.back()}>
+              <ChevronLeft size="24" />
+            </IconWrapper>
+            <Title>{title}</Title>
+          </>
+        )}
+
+      </SmallContainer>
 
       <RightIcons>
         {showMapIcon && (
           <IconWrapper onClick={() => router.push('/place')}>
-            <Place size="24" />
+            <StyledPlace size="28" />
           </IconWrapper>
         )}
         {showFavoriteIcon && (
           <IconWrapper onClick={() => router.push('/place')}>
-            <Heart size="24" />
+            <StyledHeart size="28" />
           </IconWrapper>
         )}
         <IconWrapper onClick={() => router.push("/main")}>
-          <Home size="24" />
+          <StyledHome size="28" color='black' />
         </IconWrapper>
       </RightIcons>
 
@@ -43,9 +56,18 @@ const Header = ({title, showFavoriteIcon, showMapIcon}) => {
 export default Header;
 
 const Container = styled.div`
+  width: 600px;
+  height: 50px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
+  padding: 10px 20px;
   border-bottom: 1px solid ${theme.colors.divider};
+`;
+
+const SmallContainer = styled.div`
+  display: flex;
+  align-items: center;
 `;
 
 const IconWrapper = styled.div`
@@ -53,13 +75,52 @@ const IconWrapper = styled.div`
   color: black;
 `;
 
+const LogoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+`;
+
+const LogoImage = styled(Image)`
+  width: 36px;
+  height: 36px;
+`;
+
+const LogoTitle = styled.div`
+  padding-left: 12px;
+  font-size: 20px;
+  font-family: 'Gugi', sans-serif;
+`;
+
 const Title = styled.div`
   font-size: 18px;
   font-weight: bold;
+  padding-left: 15px;
 `;
 
 const RightIcons = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
+`;
+
+const StyledPlace = styled(Place)`
+  width: 28px;
+  height: 28px;
+  stroke-width: 1; /* Adjust line thickness */
+  color: black;
+`;
+
+const StyledHeart = styled(HeartOutlined)`
+  width: 28px;
+  height: 28px;
+  stroke-width: 1; /* Adjust line thickness */
+  color: black;
+`;
+
+const StyledHome = styled(Home)`
+  width: 28px;
+  height: 28px;
+  stroke-width: 1.5; /* Adjust line thickness */
+  color: black;
 `;
--- src/components/common/Header/Header.stories.js
+++ src/components/common/Header/Header.stories.js
@@ -8,9 +8,30 @@ export default {
 
 const Template  = (args) => <Header {...args} />;
 
-export const MyPageHeader = Template.bind({});
-MyPageHeader.args = {
-  title: "마이페이지",
+export const DefaultHeader = Template.bind({});
+DefaultHeader.args = {
+  showLogo: true,
+  showFavoriteIcon: true,
+  
+}
+
+export const OnlyHomeIcon = Template.bind({});
+OnlyHomeIcon.args = {
+  title: "홈 아이콘만 있는 헤더",
   showFavoriteIcon: false,
+  showMapIcon: false
+}
+
+export const WithMapIcon = Template.bind({});
+WithMapIcon.args = {
+  title: "댕플 아이콘 + 홈 아이콘",
+  showFavoriteIcon: false,
+  showMapIcon: true
+}
+
+export const WithBookmarkIcon = Template.bind({});
+WithBookmarkIcon.args = {
+  title: "즐찾 아이콘 + 홈 아이콘",
+  showFavoriteIcon: true,
   showMapIcon: false
 }

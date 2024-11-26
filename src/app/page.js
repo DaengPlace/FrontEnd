5531e59a677fd26f4cf80d04d669d2371e2b4a79
parkyejin <uiop5809@naver.com>
2024-11-26 14:54:44 +0900

    docs(#13): SNS 계정으로 로그인 text

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,12 @@ export default function Home() {
         <Title>댕댕플레이스</Title>
       </TextBox>
 
+      <Divider>
+        <hr />
+        <span>SNS 계정으로 로그인</span>
+        <hr />
+      </Divider>
+
       <ButtonBox>
         <Image
           src="/assets/common/kakao_login_btn.svg"
@@ -36,7 +42,7 @@ const Container = styled.div`
   display: flex;
   align-items: center;
   flex-direction: column;
-  gap: 60px;
+  gap: 40px;
 
   background-image: url("/assets/common/start.svg");
   background-size: cover;
@@ -50,6 +56,24 @@ const TextBox = styled.div`
   flex-direction: column;
   gap: 15px;
   margin-top: 10rem;
+`;
+
+const Divider = styled.div`
+  display: flex;
+  align-items: center;
+  width: 50%;
+  gap: 10px;
+
+  hr {
+    flex: 1;
+    border: none;
+    border-top: 1px solid ${({ theme }) => theme.colors.white};
+  }
+  span {
+    font-size: 0.9rem;
+    color: ${({ theme }) => theme.colors.white};
+    white-space: nowrap; /* 텍스트 줄바꿈 방지 */
+  }
 `;
 
 const ButtonBox = styled.div`

65c410552d27bcec54e639829f5be218cf0d125b
김예원 <yaewon4643@naver.com>
2024-11-29 17:40:39 +0900

    Design(#31): 성향별 추천 페이지 초기 세팅

--- src/app/recommend/page.jsx
+++ src/app/recommend/page.jsx
@@ -1,5 +1,23 @@
+"use client";
+
+import Header from "@/components/common/Header/Header";
+import { WithBookmarkIcon } from "@/components/common/Header/Header.stories";
+import styled from "styled-components";
+
 const RecommendPage = () => {
-  return <div>추천페이지</div>;
+  return (
+    <Container>
+      <Header title="성향별 시설 추천" showFavoriteIcon={WithBookmarkIcon.args.showFavoriteIcon} showMapIcon={WithBookmarkIcon.args.showMapIcon} />
+    </Container>
+  );
 };
 
 export default RecommendPage;
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;

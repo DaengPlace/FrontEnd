c70c664b91f6da5c77278ffe98326ef2fc6689d3
KangBoSeok <kbs9901@gmail.com>
2024-12-03 10:24:06 +0900

    Design(#36) : 헤더 수정

--- src/components/reviews/PhotoReview/HeaderSection/HeaderSection.jsx
+++ src/components/reviews/PhotoReview/HeaderSection/HeaderSection.jsx
@@ -7,21 +7,12 @@ import { WithMapIcon } from "@/components/common/Header/Header.stories";
 
 const HeaderSection = () => {
   return (
-    <HeaderWrapper>
       <Header
         title="포토/비디오 리뷰 모아보기"
-        showFavoriteIcon={WithMapIcon.args.showFavoriteIcon}
+        showHomeIcon={WithMapIcon.args.showHomeIcon}
         showMapIcon={WithMapIcon.args.showMapIcon}
       />
-    </HeaderWrapper>
   );
 };
 
 export default HeaderSection;
-
-const HeaderWrapper = styled.div`
-  position: fixed;
-  top: 0;
-  z-index: 1000;
-  background-color: white;
-`;

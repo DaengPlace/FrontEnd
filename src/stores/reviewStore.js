ce8bf477363d0b1c19c16cb7e30816de37329400
KangBoSeok <kbs9901@gmail.com>
2024-12-08 18:16:06 +0900

    feat(#67) : zustand 카테고리 추가

--- src/stores/reviewStore.js
+++ src/stores/reviewStore.js
@@ -3,8 +3,10 @@ import { create } from 'zustand';
 const useReviewStore = create((set) => ({
   placeName: '',
   visitDate: '',
+  category: '',
   setPlaceName: (name) => set({ placeName: name }),
   setVisitDate: (date) => set({ visitDate: date }),
+  setCategory: (category) => set({category: category}),
 }));
 
 export default useReviewStore;

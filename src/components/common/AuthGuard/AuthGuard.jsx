ef95c0f5ecf4d16ade5e705677693e7ed6640453
김예원 <yaewon4643@naver.com>
2024-12-11 01:48:40 +0900

    Design(#39): AuthGuard 바텀시트 스타일 조정

--- src/components/common/AuthGuard/AuthGuard.jsx
+++ src/components/common/AuthGuard/AuthGuard.jsx
@@ -2,6 +2,7 @@ import { useAuth } from '@/contexts/AuthContext';
 import React, { useState } from 'react';
 import BottomSheet from '../BottomSheet/BottomSheet';
 import { useRouter } from 'next/navigation';
+import styled from 'styled-components';
 
 const AuthGuard = ({children}) => {
 
@@ -21,19 +22,28 @@ const AuthGuard = ({children}) => {
     <>
       {React.cloneElement(children, {onClick: handleAction})}
       {isBottomSheetOpen && (
-        <BottomSheet
-          title="로그인이 필요한 서비스입니다"
-          confirmText="로그인"
-          cancelText="닫기"
-          onConfirm={() => {
-            setIsBottomSheetOpen(false);
-            router.push('/');
-          }}
-          onClose={() => setIsBottomSheetOpen(false)}
-        />
+        <StyledBottomSheetWrapper>
+            <BottomSheet
+              title="로그인이 필요한 서비스입니다"
+              confirmText="로그인"
+              cancelText="닫기"
+              onConfirm={() => {
+                setIsBottomSheetOpen(false);
+                router.push('/');
+              }}
+              onClose={() => setIsBottomSheetOpen(false)}
+            />
+        </StyledBottomSheetWrapper>
       )}
     </>
   );
 };
 
 export default AuthGuard;
+
+
+const StyledBottomSheetWrapper = styled.div`
+  h2 {
+    margin-bottom: 30px; /* 제목과 버튼 사이 간격 */
+  }
+`;

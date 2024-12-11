bb718f45a18eda74ff88505197b0ab3ad4425f69
김예원 <yaewon46438@gmail.com>
2024-12-11 23:16:31 +0900

    Design(#73): AuthContext accesstoken 초기값 null로 변경

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -5,7 +5,7 @@ import React, { createContext, useContext, useState } from "react";
 const AuthContext = createContext();
 
 export const AuthProvider = ({children}) => {
-  const [accessToken, setAccessToken] = useState('abcd');
+  const [accessToken, setAccessToken] = useState(null);
 
   return (
     <AuthContext.Provider value={{accessToken, setAccessToken}}>

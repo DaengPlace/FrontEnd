f593b353f0c55607c6012375b9514737f537cb2d
parkyejin <uiop5809@naver.com>
2024-12-12 14:48:54 +0900

    test(#72): 카카오 소셜 로그인 401 에러 해결 전

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: NEXT_PUBLIC_APP_BASE_URL,
+  baseURL: process.env.NEXT_PUBLIC_APP_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },

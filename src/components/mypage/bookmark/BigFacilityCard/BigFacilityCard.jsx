255e0c2131d12d0b3ee767597b6544adfa08e278
김예원 <yaewon46438@gmail.com>
2024-12-18 02:07:53 +0900

    Feat(#100): 즐겨찾기 목록 조회 시 등록/해제 API 연동

--- src/components/mypage/bookmark/BigFacilityCard/BigFacilityCard.jsx
+++ src/components/mypage/bookmark/BigFacilityCard/BigFacilityCard.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import styled from "styled-components";
 import { Favorite, FavoriteBorder } from "@mui/icons-material";
 import { useRouter } from "next/navigation";
 import theme from "@/styles/theme.js";
+import { addFavorite, removeFavorite } from "@/apis/place/favorite";
 
-const BigFacilityCard = ({fac, toggleLike}) => {
+const BigFacilityCard = ({fac}) => {
 
+  const [isLiked, setIsLiked] = useState(fac.isLiked);
   const router = useRouter();
 
   const handleClick = () => {
-    router.push(`/place/placedetail?id=${fac.id}`);
+    router.push(`/place/placedetail?placeId=${fac.id}`);
+  };
+
+  const handleFavoriteClick = async (e) => {
+    e.stopPropagation(); // 이벤트 전파 차단
+    try {
+      if (isLiked) {
+        await removeFavorite(fac.id);
+      } else {
+        await addFavorite(fac.id);
+      }
+      // 상태 업데이트
+      setIsLiked((prev) => !prev);
+    } catch (error) {
+      console.error("Failed to update favorite status", error);
+    }
   };
 
   return (
@@ -18,13 +35,8 @@ const BigFacilityCard = ({fac, toggleLike}) => {
     <Container onClick={handleClick}>
       <ImageWrapper>
         <FacilityImage src={fac.image} alt={fac.name} width={510} height={300} />
-        <FavoriteButton onClick={(e) => {
-          e.stopPropagation(); // 이벤트 전파 차단
-          toggleLike(fac.id)
-          }}
-          isLiked={fac.isLiked}
-        >
-          {fac.isLiked ? (<Favorite />) : (<FavoriteBorder />)}
+        <FavoriteButton onClick={handleFavoriteClick} $isLiked={isLiked}>
+          {isLiked ? (<Favorite />) : (<FavoriteBorder />)}
         </FavoriteButton>
       </ImageWrapper>
 
@@ -74,7 +86,7 @@ const FacilityImage = styled(Image)`
 `;
 
 const FavoriteButton = styled.button.withConfig({
-  shouldForwardProp: (prop) => prop !== 'isLiked', // isLiked prop을 DOM에 전달하지 않음
+  shouldForwardProp: (prop) => !["hovered", "$isliked"].includes(prop), // isLiked prop을 DOM에 전달하지 않음
 })`
   position: absolute;
   top: 10px;
@@ -87,7 +99,7 @@ const FavoriteButton = styled.button.withConfig({
 
   svg {
     font-size: 30px;
-    color: ${({isLiked}) => (isLiked ? 'red' : 'white')};
+    color: ${({$isLiked}) => ($isLiked ? 'red' : 'white')};
     transition: transform 0.2s ease, color 0.2s ease;
   }
 
@@ -142,12 +154,6 @@ const Name = styled.div`
 const Address = styled.div`
   font-size: 14px;
   color: #777;
-  margin-bottom: 10px;
-`;
-
-const Details = styled.div`
-  font-size: 12px;
-  color: #555;
   margin-bottom: 10px;
 `;
 

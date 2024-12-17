25f9657c619abc3007a13499739b312ea9c6a003
parkyejin <uiop5809@naver.com>
2024-12-17 17:51:23 +0900

    fix(#72): 프로필 수정 formData 변경

--- src/apis/user/putUserUpdate.jsx
+++ src/apis/user/putUserUpdate.jsx
@@ -1,6 +1,8 @@
 import { axiosInstance as axios } from "@/apis/axiosInstance";
 
-export const updatePets = async (updatedData) => {
-  const response = await axios.put(`/members/profile`, updatedData);
+export const putUserUpdate = async (formData) => {
+  const response = await axios.put(`/members/profile`, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
   return response.data;
 };

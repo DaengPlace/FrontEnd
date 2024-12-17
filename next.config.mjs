98f3918ecb15ed4bfe8b96ae4f0ba342e8966aa9
KangBoSeok <kbs9901@gmail.com>
2024-12-18 02:24:45 +0900

    Docs(#102) : 이미지 호스트 추가

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,7 @@ const nextConfig = {
       "daeng-place-profile-s3.s3.ap-northeast-2.amazonaws.com",
       "img1.kakaocdn.net",
       "t1.kakaocdn.net",
+      'k.kakaocdn.net'
     ], // 이미지 호스트 추가
   },
 };
